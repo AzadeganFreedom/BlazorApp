248db6c7b636b5cd80c54bca1234ac160aa851c2
Tec <Tec@SKAB1-PC12>
2023-03-15 14:22:13 +0100

    Add project files.

---
+++ BlazorApp/Data/Meme.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace BlazorApp.Data
+{
+    
+    public class Meme
+    {
+
+        public int Id { get; set; }
+        public string MemeName { get; set; }
+        public string Url { get; set; }
+
+        
+    }
+}
